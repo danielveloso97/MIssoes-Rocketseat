622783c010bc949e17d52d9c649d382b76c94b6d
Daniel Veloso <ds799623@gmail.com>
2021-09-14 10:40:56 -0300

    add section services

---
+++ Mission-Origin/main.js
@@ -0,0 +1,19 @@
+/*Abre e fecha o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+
+for (const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+/*Clicou em um item, fecha o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
